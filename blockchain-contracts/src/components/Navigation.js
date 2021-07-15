ac594fe24b5ae0ef7456335e9444bd2d22c18acc
jeanmarcduong <jeanmarcduong98@gmail.com>
2021-07-15 15:17:11 -0400

    Nav Bar without CSS

---
+++ blockchain-contracts/src/components/Navigation.js
@@ -0,0 +1,22 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Team</a>
+        </li>
+        <li>
+          <a href="">Donations</a>
+        </li>
+        <li style="float:right">
+          <a href="">Login/Sign-in</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
