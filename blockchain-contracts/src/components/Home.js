a94ffa92c1c7d77f199bead1d5933ebdff27cd54
jeanmarcduong <jeanmarcduong98@gmail.com>
2021-07-29 13:35:14 -0400

    2/3 of home page done, needs some work on styling

--- blockchain-contracts/src/components/Home.js
+++ blockchain-contracts/src/components/Home.js
@@ -3,11 +3,11 @@ import HomeCSS from './Home.module.css';
 const Home = () => {
   return (
     <React.Fragment>
-      <div className="container-flex">
+      <div className={`container-flex ${HomeCSS.contain}`} id="container-flex">
         {/* Create account section */}
         <section>
           <div className="row my-5">
-            <div className="col infoBox text-center ">
+            <div className="col text-center ">
               <h2>Create a Cryptonate Account</h2>
               <p className="mt-5">
                 By creating an account you can track your donations, recieve
@@ -22,7 +22,7 @@ const Home = () => {
                 Create Account
               </button>
             </div>
-            <div className="col infoBox d-flex justify-content-center  ">
+            <div className="col d-flex justify-content-center  ">
               <img
                 className="img-fluid rounded mb-4 mb-lg-0"
                 src={'./placeholder.jpg'}
@@ -32,7 +32,47 @@ const Home = () => {
           </div>
         </section>
         {/* Top Campaigns Section */}
-        <section></section>
+        <section>
+          <div className="row my-5">
+            <div className="col d-flex justify-content-center  ">
+              <img
+                className="img-fluid rounded mb-4 mb-lg-0"
+                src={'./placeholder.jpg'}
+                alt=""
+              />
+            </div>
+            <div className="col  text-center ">
+              <h2 className="mt-3">Top Campaigns</h2>
+              <div>
+                <button
+                  type="button"
+                  className="row btn bg-dark mt-2 w-25"
+                  style={{ color: 'white' }}
+                >
+                  Charity 1
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="row btn bg-dark mt-2 w-25"
+                  style={{ color: 'white' }}
+                >
+                  Charity 2
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="row btn bg-dark mt-2 w-25 "
+                  style={{ color: 'white' }}
+                >
+                  Charity 3
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
         {/* How it works Section */}
         <section></section>
       </div>
