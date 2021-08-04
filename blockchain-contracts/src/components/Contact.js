6ab7ab4392682b28c5f782add360cfbd2394b0aa
Jos02378 <57058570+Jos02378@users.noreply.github.com>
2021-08-05 03:33:42 +0700

    add input validation to contact

--- blockchain-contracts/src/components/Contact.js
+++ blockchain-contracts/src/components/Contact.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Contact.module.css';
+import swal from 'sweetalert';
 
 const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    const validateEmail = (email) => {
+        const re =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    };
+
+    const success = () => {
+        swal({
+            title: 'Thank you',
+            text: 'Message Sent Successfully!',
+            icon: 'success',
+            button: 'OK!',
+        });
+    };
+
+    const emptyInput = (field) => {
+        swal({
+            title: 'Oops...!',
+            text: `Please enter your ${field}!`,
+            icon: 'error',
+            button: 'OK!',
+        });
+    };
+
+    const invalidEmail = () => {
+        swal({
+            title: 'Oops...!',
+            text: 'Please enter a valid email address',
+            icon: 'error',
+            button: 'OK!',
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (name.length < 1) {
+            emptyInput('name');
+        } else if (email.length < 1) {
+            emptyInput('email');
+        } else if (!validateEmail(email)) {
+            invalidEmail();
+        } else if (subject.length < 1) {
+            emptyInput('subject');
+        } else if (message.length < 1) {
+            emptyInput('message');
+        } else {
+            success();
+            setName('');
+            setEmail('');
+            setSubject('');
+            setMessage('');
+        }
+    };
+
     return (
         <div className={style.container}>
             <div className={style.form}>
@@ -52,38 +112,46 @@ const Contact = () => {
                 </div>
 
                 <div className={style.contactForm}>
-                    <form action='index.html' autocomplete='off'>
+                    <form action='index.html' autoComplete='off'>
                         <h3 className={style.title}>Contact us</h3>
                         <div className={style.inputContainer}>
                             <input
                                 type='text'
                                 className={style.input}
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
                                 id='name'
                                 placeholder='Enter your name.'
-                                autocomplete='off'
+                                autoComplete='off'
                             />
                         </div>
                         <div className={style.inputContainer}>
                             <input
                                 type='email'
                                 className={style.input}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 id='email'
                                 placeholder='Enter your email address.'
-                                autocomplete='off'
+                                autoComplete='off'
                             />
                         </div>
                         <div className={style.inputContainer}>
                             <input
                                 type='text'
                                 className={style.input}
+                                value={subject}
+                                onChange={(e) => setSubject(e.target.value)}
                                 id='subject'
                                 placeholder='Enter the subject.'
-                                autocomplete='off'
+                                autoComplete='off'
                             />
                         </div>
                         <div className={(style.inputContainer, style.textarea)}>
                             <textarea
                                 className={style.input}
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
                                 id='message'
                                 placeholder='Enter your message here.'
                                 data-gramm_editor='false'
@@ -93,6 +161,7 @@ const Contact = () => {
                             type='submit'
                             value='Send'
                             className={style.btn}
+                            onClick={handleSubmit}
                         />
                     </form>
                 </div>
