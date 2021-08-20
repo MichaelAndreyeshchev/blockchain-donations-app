ede1df0703ac9551c0bbf7a03a23afa8997c9f92
Jos02378 <57058570+Jos02378@users.noreply.github.com>
2021-08-20 13:35:26 +0700

    add particles in homepage

--- blockchain-contracts/src/components/Home.js
+++ blockchain-contracts/src/components/Home.js
@@ -1,52 +1,56 @@
 import React from 'react';
+import ParticleBackground from './Particles';
 import HomeCSS from './Home.module.css';
 const Home = () => {
   return (
     <React.Fragment>
+      <ParticleBackground />
       <div className={`container-flex ${HomeCSS.contain}`}>
         {/* Create account section */}
-        <section>
-          <div className="row my-5">
-            <div className="col text-center ">
+        <section className={HomeCSS.container}>
+          <div className={'row my-5' + ' ' + HomeCSS.wrapper}>
+            <div className='col text-center'>
               <h2>Create a Cryptonate Account</h2>
-              <p className="mt-5">
+              <p className='mt-5'>
                 By creating an account you can track your donations, recieve
                 e-mail updates from charities, and manage your Cryptonate
                 profile
               </p>
               <button
-                type="button"
-                className="btn bg-dark"
+                type='button'
+                className='btn bg-dark'
                 style={{ color: 'white' }}
               >
                 Create Account
               </button>
             </div>
-            <div className="col d-flex justify-content-center">
+
+            {/* <div className='col d-flex justify-content-center'>
               <img
-                className="img-fluid rounded mb-4 mb-lg-0"
+                className='img-fluid rounded mb-4 mb-lg-0'
                 src={'./placeholder.jpg'}
-                alt=""
+                alt=''
               />
-            </div>
+            </div> */}
           </div>
         </section>
+
         {/* Top Campaigns Section */}
         <section>
-          <div className="row my-5">
-            <div className="col d-flex justify-content-center  ">
+          <div className='row my-5'>
+            <div className='col d-flex justify-content-center  '>
               <img
-                className="img-fluid rounded mb-4 mb-lg-0"
+                className='img-fluid rounded mb-4 mb-lg-0'
                 src={'./placeholder.jpg'}
-                alt=""
+                alt=''
               />
             </div>
-            <div className="col  text-center ">
-              <h2 className="mt-3">Top Campaigns</h2>
+            <div className='col  text-center '>
+              <h2 className='mt-3'>Top Campaigns</h2>
               <div>
                 <button
-                  type="button"
-                  className="row btn bg-dark mt-2 w-25"
+                  type='button'
+                  className='row btn bg-dark mt-2 w-25'
                   style={{ color: 'white' }}
                 >
                   Charity 1
@@ -54,8 +58,8 @@ const Home = () => {
               </div>
               <div>
                 <button
-                  type="button"
-                  className="row btn bg-dark mt-2 w-25"
+                  type='button'
+                  className='row btn bg-dark mt-2 w-25'
                   style={{ color: 'white' }}
                 >
                   Charity 2
@@ -63,8 +67,8 @@ const Home = () => {
               </div>
               <div>
                 <button
-                  type="button"
-                  className="row btn bg-dark mt-2 w-25 "
+                  type='button'
+                  className='row btn bg-dark mt-2 w-25 '
                   style={{ color: 'white' }}
                 >
                   Charity 3
@@ -73,6 +77,7 @@ const Home = () => {
             </div>
           </div>
         </section>
+
         {/* How it works Section */}
         <section></section>
       </div>
