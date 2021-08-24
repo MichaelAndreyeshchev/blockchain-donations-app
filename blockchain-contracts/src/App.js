7c883213e54a198c3c8e705c46ffedba3d4b79c6
Jos02378 <57058570+Jos02378@users.noreply.github.com>
2021-08-24 07:03:14 +0700

    refactor files

--- blockchain-contracts/src/App.js
+++ blockchain-contracts/src/App.js
@@ -1,51 +1,55 @@
-import AccountInfo from './components/AccountInfo';
-import Contact from './components/Contact';
-import CreateDonation from './components/CreateDonation';
-import Home from './components/Home';
-import Navigation from './components/Navigation';
-import SpecificDonation from './components/SpecificDonation';
-import TeamPage from './components/TeamPage';
-import PortalPage from './components/PortalPage';
-import PreviousDonations from './components/PreviousDonations';
-import DonationStatistics from './components/DonationStatistics';
+import AccountInfo from './components/Account Info/AccountInfo';
+import Contact from './components/Contact/Contact';
+import CreateDonation from './components/Create Donation/CreateDonation';
+import Home from './components/Home/Home';
+import Navigation from './components/Navigation/Navigation';
+import SpecificDonation from './components/Campaign Page/SpecificDonation';
+import TeamPage from './components/Team Page/TeamPage';
+import PortalPage from './components/Portal Page/PortalPage';
+import PreviousDonations from './components/Previous Donation/PreviousDonations';
+import DonationStatistics from './components/Donation Statistics/DonationStatistics';
 import signin from './components/signin';
 import signup from './components/signup';
-import donationlisst from './components/donationlisst';
-import Footer from './components/Footer';
-import faq from './components/faq';
+import donationlisst from './components/Donation List/donationlisst';
+import Footer from './components/Footer/Footer';
+import faq from './components/Faq/faq';
 import newSignIn from './components/newSignIn';
-import CreateCampaign from './components/CreateCampaign';
+import CreateCampaign from './components/Create Campaign/CreateCampaign';
 import logOut from './components/logOut';
-import Profile from './components/Profile';
+import Profile from './components/Profile/Profile';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-    return (
-        <div className='App'>
-            <Router>
-                <Navigation />
-                <Route exact path='/' component={Home} />
-                <Route exact path='/contact' component={Contact} />
-                <Route exact path='/teampage' component={TeamPage} />
-                <Route exact path='/accountinfo' component={AccountInfo} />
-                <Route exact path='/specificdonation' component={SpecificDonation}/>
-                <Route exact path='/signin' component={signin} />
-                <Route exact path='/signup' component={signup} />
-                <Route exact path='/donationlisst' component={donationlisst} />
-                <Route exact path='/portalpage' component={PortalPage} />
-                <Route exact path ='/DonationStatistics' component = {DonationStatistics} />
-                <Route exact path ='/PreviousDonations' component = {PreviousDonations} />
-                <Route exact path='/faq' component={faq} />
-                <Route exact path = '/CreateDonation' component = {CreateDonation}/>
-                <Route exact path = '/CreateCampaign' component = {CreateCampaign}/>
-                <Route exact path = '/newSignIn' component = {newSignIn}/>
-                <Route exact path = '/logOut' component = {logOut}/>
-                <Route exact path = '/Profile' component = {Profile}/>
-                <Route exact path='/CreateCampaign' component={CreateCampaign} />
-            </Router>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className='App'>
+      <Router>
+        <Navigation />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/teampage' component={TeamPage} />
+        <Route exact path='/accountinfo' component={AccountInfo} />
+        <Route exact path='/specificdonation' component={SpecificDonation} />
+        <Route exact path='/signin' component={signin} />
+        <Route exact path='/signup' component={signup} />
+        <Route exact path='/donationlisst' component={donationlisst} />
+        <Route exact path='/portalpage' component={PortalPage} />
+        <Route
+          exact
+          path='/DonationStatistics'
+          component={DonationStatistics}
+        />
+        <Route exact path='/PreviousDonations' component={PreviousDonations} />
+        <Route exact path='/faq' component={faq} />
+        <Route exact path='/CreateDonation' component={CreateDonation} />
+        <Route exact path='/CreateCampaign' component={CreateCampaign} />
+        <Route exact path='/newSignIn' component={newSignIn} />
+        <Route exact path='/logOut' component={logOut} />
+        <Route exact path='/Profile' component={Profile} />
+        {/* <Route exact path='/CreateCampaign' component={CreateCampaign} /> */}
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
